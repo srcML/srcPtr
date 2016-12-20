014dbd458a1849d50079198bfd2358e873d9cd18
vzyrianov <vzyriano@kent.edu>
2016-12-20 17:33:08 -0500

    Adding tests for srcPtrUtilities.hpp

---
+++ test/TestsrcPtrUtilities.cpp
@@ -0,0 +1,96 @@
+#include <DeclTypePolicy.hpp>
+#include <ParamTypePolicy.hpp>
+
+#include <srcPtrUtilities.hpp>
+
+#include <cassert>
+#include <vector>
+#include <string>
+
+void TestSrcPtrVar() {
+	{
+		std::vector<std::string> s;
+		s.push_back("std");
+
+		DeclTypePolicy::DeclTypeData data;
+		data.nameoftype = "i";
+		data.nameofidentifier = "int";
+		data.namespaces = s;
+		data.linenumber = 12;
+		data.isConst = true;
+		data.isReference = false;
+		data.isPointer = true;
+		data.isStatic = false;
+
+		srcPtrVar x(data);
+
+		assert(x == data);
+	}
+}
+
+void TestDeclStack() {
+   {
+      srcPtrVar var1;
+      var1.nameofidentifier = "var1";
+      var1.linenumber = 1;
+
+      srcPtrVar var2;
+      var2.nameofidentifier = "var2";
+      var2.linenumber = 2;
+
+      srcPtrVar var3;
+      var3.nameofidentifier = "var3";
+      var3.linenumber = 3;
+
+      srcPtrDeclStack stack;
+      stack.CreateFrame();
+      stack.AddVarToFrame(var1);
+      stack.AddVarToFrame(var2);
+      stack.AddVarToFrame(var3);
+
+      assert(stack.GetPreviousOccurence("var1") == var1);
+      assert(stack.GetPreviousOccurence("var2") == var2);
+      assert(stack.GetPreviousOccurence("var3") == var3);
+   }
+   {
+      srcPtrVar var1;
+      var1.nameofidentifier = "var";
+      var1.linenumber = 1;
+
+      srcPtrVar var2;
+      var2.nameofidentifier = "var";
+      var2.linenumber = 2;
+
+      srcPtrVar var3;
+      var3.nameofidentifier = "var";
+      var3.linenumber = 3;
+
+      srcPtrDeclStack stack;
+      stack.CreateFrame(var1);
+      stack.CreateFrame(var2);
+      stack.CreateFrame(var3);
+      stack.AddVarToFrame(var3);
+
+      assert(stack.GetPreviousOccurence("var") == var3);
+      stack.PopFrame();
+
+      srcPtrVar var4;
+      var4.nameofidentifier = "var4";
+      var4.linenumber = 10;
+
+      stack.AddVarToFrame(var4);
+      stack.CreateFrame();
+      stack.PopFrame();
+
+      assert(stack.GetPreviousOccurence("var") == var2);
+      assert(stack.GetPreviousOccurence("var4") == var4);
+      stack.PopFrame();
+      assert(stack.GetPreviousOccurence("var") == var1);
+   }
+}
+
+int main() {
+	TestSrcPtrVar();
+   TestDeclStack();
+   return 0;
+}
