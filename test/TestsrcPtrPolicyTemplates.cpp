cd5d9dd457471bb842ed965a82622545e7158a4e
vzyrianov <vzyriano@kent.edu>
2017-06-03 17:45:30 -0400

    Adding TestsrcPtrPolicyTemplates.cpp

---
+++ test/TestsrcPtrPolicyTemplates.cpp
@@ -0,0 +1,16 @@
+#include <srcPtrUtilities.hpp>
+
+#include <cassert>
+#include <vector>
+#include <string>
+
+void TestAndersen() {
+   {
+      
+   }
+}
+
+int main() {
+   TestAndersen();
+   return 0;
+}
