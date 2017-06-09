e3e3e3277f8877c874339f5965fb551bd9938841
vzyrianov <vzyriano@kent.edu>
2017-06-08 20:38:33 -0700

    Adding more tests for Andersen's algorithm

--- test/TestsrcPtrPolicyTemplates.cpp
+++ test/TestsrcPtrPolicyTemplates.cpp
@@ -4,6 +4,7 @@
 #include <cassert>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 void TestAndersen() {
    {
@@ -20,7 +21,7 @@ void TestAndersen() {
       andersen.AddAssignmentRelationship(ptr1, ptr2);
       andersen.AddAssignmentRelationship(ptr2, ptr1);
 
-      andersen.Print();
+      andersen.Finalize();
    }
    {
       srcPtrAndersen andersen;
@@ -41,7 +42,38 @@ void TestAndersen() {
       andersen.AddAssignmentRelationship(ptr2, ptr1);
       andersen.AddAssignmentRelationship(ptr1, temp);
 
-      andersen.Print();
+      andersen.Finalize();
+   }
+   {
+      srcPtrAndersen andersen;
+
+      Variable ptr1;
+      ptr1.nameofidentifier = "ptr1";
+      ptr1.isPointer = true;
+
+      Variable ptr2;
+      ptr2.nameofidentifier = "ptr2";
+      ptr2.isPointer = true;
+
+
+      Variable var1;
+      var1.nameofidentifier = "var1";
+
+      Variable var2;
+      var2.nameofidentifier = "var2";
+
+      andersen.AddPointsToRelationship(ptr1, var1);
+      andersen.AddPointsToRelationship(ptr2, var2);
+      andersen.AddAssignmentRelationship(ptr2, ptr1);
+
+      andersen.Finalize();
+
+      assert(andersen.GetPointsTo(ptr1)[0].nameofidentifier == "var1");
+
+      std::vector<Variable> ptr2PointsTo = andersen.GetPointsTo(ptr2);
+
+      assert(std::find(ptr2PointsTo.begin(), ptr2PointsTo.end(), var1) != ptr2PointsTo.end());
+      assert(std::find(ptr2PointsTo.begin(), ptr2PointsTo.end(), var2) != ptr2PointsTo.end());
    }
 }
 
