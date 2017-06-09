e433ab9f4cbff382acaf363c9cdb35eae1e224af
vzyrianov <vzyriano@kent.edu>
2017-06-08 19:58:15 -0700

    Adding tests for Andersen's algorithm

--- test/TestsrcPtrPolicyTemplates.cpp
+++ test/TestsrcPtrPolicyTemplates.cpp
@@ -1,4 +1,5 @@
 #include <srcPtrUtilities.hpp>
+#include <srcPtrPolicyTemplates.hpp>
 
 #include <cassert>
 #include <vector>
@@ -6,7 +7,41 @@
 
 void TestAndersen() {
    {
-      
+      srcPtrAndersen andersen;
+
+      Variable ptr1;
+      ptr1.nameofidentifier = "x";
+      ptr1.isPointer = true;
+
+      Variable ptr2;
+      ptr2.nameofidentifier = "y";
+      ptr2.isPointer = true;
+
+      andersen.AddAssignmentRelationship(ptr1, ptr2);
+      andersen.AddAssignmentRelationship(ptr2, ptr1);
+
+      andersen.Print();
+   }
+   {
+      srcPtrAndersen andersen;
+
+      Variable ptr1;
+      ptr1.nameofidentifier = "x";
+      ptr1.isPointer = true;
+
+      Variable ptr2;
+      ptr2.nameofidentifier = "y";
+      ptr2.isPointer = true;
+
+      Variable temp;
+      temp.nameofidentifier = "temp";
+      temp.isPointer = true;
+
+      andersen.AddAssignmentRelationship(temp, ptr2);
+      andersen.AddAssignmentRelationship(ptr2, ptr1);
+      andersen.AddAssignmentRelationship(ptr1, temp);
+
+      andersen.Print();
    }
 }
 
