d8fd93796c75934e0aeb7f3c547e48a76b5861fc
vzyrianov <vzyriano@kent.edu>
2018-09-24 17:21:02 -0400

    Add type of parameter test

--- test/TestsrcPtrDeclPolicy.cpp
+++ test/TestsrcPtrDeclPolicy.cpp
@@ -14,59 +14,60 @@
 #include <srcml.h>
 
 std::string StringToSrcML(std::string str){
-	struct srcml_archive* archive;
-	struct srcml_unit* unit;
-	size_t size = 0;
+   struct srcml_archive* archive;
+   struct srcml_unit* unit;
+   size_t size = 0;
 
-	char * ch;
+   char * ch;
 
-	archive = srcml_archive_create();
-	srcml_archive_enable_option(archive, SRCML_OPTION_POSITION);
-	srcml_archive_write_open_memory(archive, &ch, &size);
+   archive = srcml_archive_create();
+   srcml_archive_enable_option(archive, SRCML_OPTION_POSITION);
+   srcml_archive_write_open_memory(archive, &ch, &size);
 
-	unit = srcml_unit_create(archive);
-	srcml_unit_set_language(unit, SRCML_LANGUAGE_CXX);
-	srcml_unit_set_filename(unit, "testsrcType.cpp");
+   unit = srcml_unit_create(archive);
+   srcml_unit_set_language(unit, SRCML_LANGUAGE_CXX);
+   srcml_unit_set_filename(unit, "testsrcType.cpp");
 
-	srcml_unit_parse_memory(unit, str.c_str(), str.size());
-	srcml_archive_write_unit(archive, unit);
+   srcml_unit_parse_memory(unit, str.c_str(), str.size());
+   srcml_archive_write_unit(archive, unit);
 
-	srcml_unit_free(unit);
-	srcml_archive_close(archive);
-	srcml_archive_free(archive);
+   srcml_unit_free(unit);
+   srcml_archive_close(archive);
+   srcml_archive_free(archive);
 
-	std::string srcml;
-	srcml.append(ch, size);
+   std::string srcml;
+   srcml.append(ch, size);
 
-	return srcml;
+   return srcml;
 }
 
 srcPtrDeclPolicy::srcPtrDeclData Analyze(std::string codestr) {
-	std::string srcmlstr = StringToSrcML(codestr);
+   std::string srcmlstr = StringToSrcML(codestr);
 
-	srcPtrDeclPolicy *declpolicy = new srcPtrDeclPolicy();
-	srcSAXController control(srcmlstr);
-	srcSAXEventDispatch::srcSAXEventDispatcher<> handler{declpolicy};
-	control.parse(&handler);
-	
-	return declpolicy->GetData();
+   srcPtrDeclPolicy *declpolicy = new srcPtrDeclPolicy();
+   srcSAXController control(srcmlstr);
+   srcSAXEventDispatch::srcSAXEventDispatcher<> handler{declpolicy};
+   control.parse(&handler);
+
+   return declpolicy->GetData();
 }
 
 void RunTests() {
-	{
-		srcPtrDeclPolicy::srcPtrDeclData data = Analyze("int main() {\nint x;\nint * y;\ny = &x;\n}");
-		assert(data.functionTracker.GetFunction("main", 0).name == "main");
-	}
-	{
-		srcPtrDeclPolicy::srcPtrDeclData data = Analyze("int f(int * x) { return 0; } int main() {return 0;}");
-		assert(data.functionTracker.GetFunction("f", 1).name == "f");
-		assert(data.functionTracker.GetFunction("f", 1).parameters[0].nameofidentifier == "x");
-		assert(data.functionTracker.GetFunction("f", 1).parameters[0].isPointer == true);
-	}
-	std::cout << std::endl << "Finished Testing srcPtrDeclPolicy" << std::endl;
+   {
+      srcPtrDeclPolicy::srcPtrDeclData data = Analyze("int main() {\nint x;\nint * y;\ny = &x;\n}");
+      assert(data.functionTracker.GetFunction("main", 0).name == "main");
+   }
+   {
+      srcPtrDeclPolicy::srcPtrDeclData data = Analyze("int f(int * x) { return 0; } int main() {return 0;}");
+      assert(data.functionTracker.GetFunction("f", 1).name == "f");
+      assert(data.functionTracker.GetFunction("f", 1).parameters[0].nameofidentifier == "x");
+      assert(data.functionTracker.GetFunction("f", 1).parameters[0].isPointer == true);
+      assert(data.functionTracker.GetFunction("f", 1).parameters[0].nameoftype == "int");
+   }
+   std::cout << std::endl << "Finished Testing srcPtrDeclPolicy" << std::endl;
 }
 
 int main() {
-	RunTests();
-	return 0;
+   RunTests();
+   return 0;
 }
