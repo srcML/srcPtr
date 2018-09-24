eb49f16053e85b4ef8a524ac86ebdbfbe1cb3e40
vzyrianov <vzyriano@kent.edu>
2018-09-23 20:40:42 -0400

    Add test for DisjointSet

--- test/TestDisjointSet.cpp
+++ test/TestDisjointSet.cpp
@@ -2,9 +2,47 @@
 #include <cassert>
 
 void test_disjoint_set() {
-   DisjointSet ds;
+   Variable var1;
+   Variable var2;
+   Variable var3;
 
-   
+   var1.linenumber = 1;
+   var1.nameofidentifier = "var1";
+   var1.nameoftype = "int";
+
+   var2.linenumber = 2;
+   var2.nameofidentifier = "var2";
+   var2.nameoftype = "int";
+
+   var3.linenumber = 3;
+   var3.nameofidentifier = "var3";
+   var3.nameoftype = "int";
+
+
+   {
+      DisjointSet ds;
+
+      ds.MakeSet(var1);
+      ds.MakeSet(var2);
+
+      assert(ds.GetSet(var1) != ds.GetSet(var2));
+      assert(ds.Find(var1) != ds.Find(var2));
+
+      ds.Union(var1, var2);
+
+      assert(ds.Find(var1) == ds.Find(var2));
+
+      ds.MakeSet(var3);
+
+      assert(ds.Find(var3) != ds.Find(var2));
+
+      ds.Union(var3, var1);
+
+      assert(ds.Find(var1) == ds.Find(var2));
+      assert(ds.Find(var2) == ds.Find(var3));
+
+      assert(ds.GetSet(var1) == ds.GetSet(var2));
+   }
 }
 
 int main() {
